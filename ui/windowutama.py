b6651e360f970155f89906664930cdf4b58f1f4f
Lenanorliana <lianalena742001@gmail.com>
2024-11-13 21:56:09 +0800

    liana
    uts visual 3

---
+++ ui/windowutama.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+from PyQt6 import uic
+
+# Kelas untuk menghubungkan GUI dari file .ui
+class FORM_UI(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("windowUtama.ui", self)  # Memuat file .ui
+
+# Membuat aplikasi dan menampilkan jendela
+if __name__ == "__main__":
+    app = QApplication(sys.argv)  # Membuat instance aplikasi
+    window = FORM_UI()             # Membuat instance jendela utama
+    window.show()                  # Menampilkan jendela
+    sys.exit(app.exec())           # Menjalankan event loop
